62ead8f85ad3a4376d97ea57170da0b341eb692a
Panji Dwi Setiawan <panji2setiawan@icloud.com>
2020-09-19 23:26:54 +0900

    add post

--- api/controllers/base.go
+++ api/controllers/base.go
@@ -56,7 +56,7 @@ func (server *Server) Initialize(Dbdriver, DbUser, DbPassword, DbPort, DbHost, D
 		server.DB.Exec("PRAGMA foreign_keys = ON")
 	}
 
-	server.DB.Debug().AutoMigrate(&models.User{}) //database migration
+	server.DB.Debug().AutoMigrate(&models.User{}, &models.Post{}) //database migration
 
 	server.Router = mux.NewRouter()
 
--- api/seed/seeder.go
+++ api/seed/seeder.go
@@ -18,28 +18,47 @@ var users = []models.User{
 		Email:    "nadia.slavina@gmail.com",
 		Password: "password",
 	},
-	models.User{
-		Nickname: "Madog Hernades",
-		Email:    "mad.dog99@gmail.com",
-		Password: "password",
+}
+
+var posts = []models.Post{
+	models.Post{
+		Title:   "Title 1",
+		Content: "Hello world 1",
+	},
+	models.Post{
+		Title:   "Title 2",
+		Content: "Hello world 2",
 	},
 }
 
 func Load(db *gorm.DB) {
 
-	err := db.Debug().DropTableIfExists(&models.User{}).Error
+	err := db.Debug().DropTableIfExists(&models.Post{}, &models.User{}).Error
 	if err != nil {
 		log.Fatalf("cannot drop table: %v", err)
 	}
-	err = db.Debug().AutoMigrate(&models.User{}).Error
+	err = db.Debug().AutoMigrate(&models.User{}, &models.Post{}).Error
 	if err != nil {
 		log.Fatalf("cannot migrate table: %v", err)
 	}
+
+	/*
+		err = db.Debug().Model(&models.Post{}).AddForeignKey("author_id", "users(id)", "cascade", "cascade").Error
+		if err != nil {
+			log.Fatalf("attaching foreign key error: %v", err)
+		}
+	*/
 
 	for i, _ := range users {
 		err = db.Debug().Model(&models.User{}).Create(&users[i]).Error
 		if err != nil {
 			log.Fatalf("cannot seed users table: %v", err)
+		}
+		posts[i].AuthorID = users[i].ID
+
+		err = db.Debug().Model(&models.Post{}).Create(&posts[i]).Error
+		if err != nil {
+			log.Fatalf("cannot seed posts table: %v", err)
 		}
 	}
 }
